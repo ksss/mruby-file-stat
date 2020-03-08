5e3fcea73d637a9b848ab85783afb17629462c7f
Takashi Kokubun <takashikkbn@gmail.com>
2020-03-08 13:02:48 -0700

    Test cross compile on mruby-cli

--- .travis_build_config.rb
+++ .travis_build_config.rb
@@ -1,7 +1,33 @@
+def gem_config(conf)
+  conf.gembox 'default'
+  conf.gem '../mruby-file-stat'
+end
+
 MRuby::Build.new do |conf|
   toolchain :gcc
-  conf.gembox 'default'
   conf.enable_test
 
-  conf.gem '../mruby-file-stat'
+  gem_config(conf)
+end
+
+if ENV['TARGET'] == 'windows-x86_64'
+  MRuby::CrossBuild.new('windows-x86_64') do |conf|
+    toolchain :gcc
+
+    conf.cc.command       = 'x86_64-w64-mingw32-gcc'
+    conf.linker.command   = 'x86_64-w64-mingw32-gcc'
+    conf.cxx.command      = 'x86_64-w64-mingw32-g++'
+    conf.archiver.command = 'x86_64-w64-mingw32-gcc-ar'
+
+    conf.exts do |exts|
+      exts.object = '.obj'
+      exts.executable = '.exe'
+      exts.library = '.lib'
+    end
+
+    conf.build_target     = 'x86_64-pc-linux-gnu'
+    conf.host_target      = 'x86_64-w64-mingw32'
+
+    gem_config(conf)
+  end
 end
